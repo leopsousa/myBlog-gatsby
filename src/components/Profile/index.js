b205a84926105f0ba100368131566ef509c0683f
Leandro Pereira de Sousa <leo.sousa3108@gmail.com>
2020-07-11 19:02:21 -0300

    component Avatar

---
+++ src/components/Profile/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Profile = () => {
+  return (
+    <div className="Profile-wrapper">
+      <h1>Léo Sousa</h1>
+      <h2>Programador front-end, empreendedor e artista</h2>
+      <p>
+        Programador junior na empresa Beneb App, apaixonado por empreendimento e
+        investimento, CEO da MRLmalharia, fundador da desencoder e nas horas
+        vagas sou design, tento as vezes fazer desenhos a lápis e digital.
+      </p>
+    </div>
+  )
+}
+
+export default Profile
