43e9ba55955e3594bbe2e0b32659aae1b7350524
Leandro Pereira de Sousa <leo.sousa3108@gmail.com>
2020-07-13 19:09:32 -0300

    post item props

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,27 +1,43 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 import * as styles from "./styles"
 
-const PostItem = () => {
+const PostItem = ({
+  slug,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+  background,
+}) => {
   return (
-    <styles.PostItemLink>
+    <styles.PostItemLink to={slug}>
       <styles.PostItemWrapper>
-        <styles.PostItemTag background="#333">Misc</styles.PostItemTag>
+        <styles.PostItemTag background={background}>
+          {category}
+        </styles.PostItemTag>
         <styles.PostItemInfo>
           <styles.PostItemDate>
-            30 de julho de 2019 ° 4min de leitura
+            {date} {timeToRead}
           </styles.PostItemDate>
-          <styles.PostItemTitle>
-            Diga não ao medium: tenha sua própria plataforma
-          </styles.PostItemTitle>
-          <styles.PostItemDescription>
-            Algumas razões para você ter sua própria plataforma ao invés de
-            soluções como o Medium
-          </styles.PostItemDescription>
+          <styles.PostItemTitle>{title}</styles.PostItemTitle>
+          <styles.PostItemDescription>{description}</styles.PostItemDescription>
         </styles.PostItemInfo>
       </styles.PostItemWrapper>
     </styles.PostItemLink>
   )
+}
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 }
 
 export default PostItem
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,32 +7,15 @@ import PostItem from "../components/PostItem"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
-    <PostItem />
+    <PostItem
+      slug="/about"
+      background="#145aa5"
+      category="Art"
+      date="10 de julho de 2020"
+      timeToRead="5"
+      title="Minha primeira arte"
+      description="Minha paixão sempre foi esse lado artisitco confira como foi feita minha primeira arte"
+    />
   </Layout>
 )
 
