ee3c07e3db10f2d8b7bd55e9a77386e5e8189589
Leandro Pereira de Sousa <leo.sousa3108@gmail.com>
2020-07-14 10:09:09 -0300

    markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -30,6 +31,21 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
+
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
