87c908bb1a7323d0c1eee9293519b3b0bca1269a
Leandro Pereira de Sousa <leo.sousa3108@gmail.com>
2020-07-25 18:29:28 -0300

    styles post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,31 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+import * as styles from "../components/Post/styles"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <>
-      <h1>Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
+
+      <styles.PostHeader>
+        <styles.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura.
+        </styles.PostDate>
+        <styles.PostTitle>{post.frontmatter.title}</styles.PostTitle>
+        <styles.PostDescription>
+          {post.frontmatter.description}
+        </styles.PostDescription>
+      </styles.PostHeader>
+      <styles.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </styles.MainContent>
+    </Layout>
   )
 }
 
@@ -17,8 +34,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
